3a55ca2a323d3104cba36098709280b1b95fd402
lichee03 <129747059+lichee03@users.noreply.github.com>
2023-10-17 16:55:36 +0800

    Update AdminPanel.java

--- AdminPanel.java
+++ AdminPanel.java
@@ -1,36 +1,51 @@
-/*
+package WallStreet;/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminPanel { //link to User authentification
     private List<User> users;
     private TradingEngine tradingEngine;
     private PortFolio portfolio;
-    private Database database= new Database();
+    private Database database = new Database();
 
     public AdminPanel() {
         users = new ArrayList<>();
     }
 
-    public boolean CheckShortSell(Stock stock, PortFolio portfolio) {
-        boolean shortsell = false;
-        int currentshares = portfolio.getHoldings().getOrDefault(stock, 0);
+    public void userList() {
+        users = database.retriveUserList();
+        List<User> userList = database.retriveUserList();
 
+        List<User> usersToRemove = new ArrayList<>();
 
-        return shortsell;
-    }
+        for (User user : userList) {
+            if (user.getName().equalsIgnoreCase("Admin")) {
+                usersToRemove.add(user);
+            }
+        }
 
-    public boolean CheckMarginTrade() {
-        boolean marginTrade = false;
-        //method
-        return marginTrade;
+        userList.removeAll(usersToRemove);
+        System.out.println("User list");
+        System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.printf("%-20s %-20s %-20s %-20s \n", " Name", "Email", "Accountbalance", "Status");
+        System.out.println("--------------------------------------------------------------------------------------------");
+        if (users.isEmpty()) {
+            System.out.printf("%-20s %-30s %-20s %-20s\n", "null", "null", "null", "null");
+        }
+        for (User user : userList) {
+            System.out.printf("%-20s %-20s %-20s %-20s\n", user.getName(), user.getEmail(), user.getAccountBalance(), user.getStatus());
+        }
+
     }
 
 
     public void disqualifyUser(String username) {
-        users=database.retriveUserList();
+        users = database.retriveUserList();
         for (User user : users) {
             if (user.getName().equals(username)) {
                 user.Disqualified();
@@ -40,17 +55,21 @@ public class AdminPanel { //link to User authentification
     }
 
 
-
     public void checkAccountBalance() {
-        users=database.retriveUserList();
+        users = database.retriveUserList();
         for (User user : users) {
-            if (user.getStatus()!="disqualified") {
-                portfolio= new PortFolio(user);
+            if (user.getStatus() != "disqualified") {
+                portfolio = new PortFolio(user);
                 double accountBalance = portfolio.getAccountBalance();
-                if (accountBalance >= 0.5 * 500000&& accountBalance<0) {
+                if (accountBalance >= 0.5 * 500000 && accountBalance < 0) {
                     disqualifyUser(user.getName());
                 }
             }
         }
     }
+
+
+
+
+
 }
